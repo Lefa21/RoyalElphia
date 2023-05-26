cb882ba3311263affa87457d746c2144985ab97e
aboudjemai <aboudjemai@iut.univ-paris8.fr>
2023-05-26 16:07:04 +0200

    ajout des classes tour et modification du fxml

--- src/main/java/fr/montreuil/iut/RoyalElphia/Controller/HelloController.java
+++ src/main/java/fr/montreuil/iut/RoyalElphia/Controller/HelloController.java
@@ -2,10 +2,13 @@ package fr.montreuil.iut.RoyalElphia.Controller;
 
 import fr.montreuil.iut.RoyalElphia.Vue.TerrainVue;
 import fr.montreuil.iut.RoyalElphia.Vue.VueEnnemi;
-import fr.montreuil.iut.RoyalElphia.modele.Ennemis;
-import fr.montreuil.iut.RoyalElphia.modele.Terrain;
-import fr.montreuil.iut.RoyalElphia.modele.jeu;
+import fr.montreuil.iut.RoyalElphia.modele.*;
 import javafx.collections.ListChangeListener;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -14,16 +17,21 @@ import javafx.fxml.Initializable;
 import javafx.scene.layout.TilePane;
 import javafx.util.Duration;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 public class HelloController implements Initializable {
 
- private Terrain terrain;
-@FXML
- private TilePane map;
+    @FXML
+    public Label LabelArgent;
 
-private jeu jeu;
+    private Terrain terrain;
+    @FXML
+    private TilePane map;
+
+    private jeu jeu;
 
     @FXML
     private Pane panneauJeu;
@@ -34,6 +42,22 @@ private jeu jeu;
 
     private VueEnnemi vueEnnemi;
 
+    private Tour tour;
+
+    private ArrayList<Tour> listeTour = new ArrayList<>();
+
+    public void CliqueTourABombe(MouseEvent mouseEvent) throws FileNotFoundException {
+        System.out.println("tour cliqué");
+        this.tour = new TourABombe();
+        listeTour.add(this.tour);
+        Image tour = new Image(new FileInputStream("src/main/resources/fr/montreuil/iut/RoyalElphia/tt-PhotoRoom.png-PhotoRoom.png"));
+        ImageView tour1 = new ImageView(tour);
+        tour1.setFitWidth(80);
+        tour1.setFitHeight(95);
+        tour1.setX(25 + (int)(Math.random() * ((540- 25) + 1)));
+        tour1.setY(25 + (int)(Math.random() * ((540- 25) + 1)));
+        panneauJeu.getChildren().add(tour1);
+    }
 
 
 private void initAnimation() {
@@ -44,7 +68,7 @@ private void initAnimation() {
 
     KeyFrame kf = new KeyFrame(
             // on définit le FPS (nbre de frame par seconde)
-            Duration.seconds(0.03),
+            Duration.seconds(0.3),
             // on définit ce qui se passe à chaque frame
             // c'est un eventHandler d'ou le lambda
             (ev ->{
@@ -106,4 +130,5 @@ private void initAnimation() {
         }
 
     }
+
 }
--- src/main/java/fr/montreuil/iut/RoyalElphia/HelloApplication.java
+++ src/main/java/fr/montreuil/iut/RoyalElphia/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Terrain.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 640);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 740);
+        stage.setTitle("RoyalElphia");
         stage.setScene(scene);
         stage.show();
     }
