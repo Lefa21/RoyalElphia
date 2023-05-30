97c1ef09b5fc609baff57b803f3e6a6df9daf860
Lefa21 <faelben@outlook.fr>
2023-05-30 21:00:35 +0200

    Modification Liste observable ennemis

--- src/main/java/fr/montreuil/iut/RoyalElphia/Controller/HelloController.java
+++ src/main/java/fr/montreuil/iut/RoyalElphia/Controller/HelloController.java
@@ -3,6 +3,7 @@ package fr.montreuil.iut.RoyalElphia.Controller;
 import fr.montreuil.iut.RoyalElphia.Vue.*;
 import fr.montreuil.iut.RoyalElphia.modele.*;
 import fr.montreuil.iut.RoyalElphia.modele.Ennemis.Ennemis;
+import fr.montreuil.iut.RoyalElphia.modele.Ennemis.ListObsEnnemis;
 import fr.montreuil.iut.RoyalElphia.modele.Map.Terrain;
 import fr.montreuil.iut.RoyalElphia.modele.Tour.Tour;
 import fr.montreuil.iut.RoyalElphia.modele.Tour.TourABombe;
@@ -71,38 +72,17 @@ private jeu jeu;
             this.LabelPV.textProperty().bind(this.jeu.getPvJoueurProperty().asString());
             TerrainVue terrainVue = new TerrainVue(terrain,map);
 
-            // demarre l'animation
+            //demarre l'animation
 
             jeu.initAnimation();
-            ListChangeListener<Ennemis> listenerEnnemis = (c -> {
-
-                while (c.next()) {
-                    if (c.wasAdded()) {
-                        for (Ennemis a : c.getAddedSubList()
-                        ) {
-                            VueEnnemi vueEnm = new VueEnnemi(panneauJeu);
-                            vueEnm.créerSprite(a);
-                        }
-                    } else if (c.wasRemoved()) {
-                        for (Ennemis a : c.getRemoved()
-                        ) {
-
-                            jeu.ajoutEnnemisMort(a);
-                            panneauJeu.getChildren().remove((panneauJeu.lookup("#" + a.getId())));
-                        }
-                    }
-                }
-            });
-
+            ListChangeListener<Ennemis> listenerEnnemis = new ListObsEnnemis(this.jeu,this.panneauJeu);
             jeu.getEnnemis().addListener(listenerEnnemis);
             this.LabelArgent.textProperty().bind(this.jeu.getArgentProperty().asString());
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-
     }
-
 
     @FXML
     public void Demarrer(Event event) {
---
+++ src/main/java/fr/montreuil/iut/RoyalElphia/modele/Ennemis/ListObsEnnemis.java
@@ -0,0 +1,38 @@
+package fr.montreuil.iut.RoyalElphia.modele.Ennemis;
+
+import fr.montreuil.iut.RoyalElphia.Vue.VueEnnemi;
+import fr.montreuil.iut.RoyalElphia.modele.jeu;
+import javafx.collections.ListChangeListener;
+import javafx.scene.layout.Pane;
+
+public class ListObsEnnemis implements ListChangeListener<Ennemis> {
+    private Pane panneauJeu;
+    private jeu jeu;
+
+    public ListObsEnnemis(jeu jeu,Pane panneauJeu){
+        this.panneauJeu = panneauJeu;
+        this.jeu = jeu;
+    }
+
+    @Override
+    public void onChanged(Change<? extends Ennemis> c) {
+        System.out.println("changement");
+        while (c.next()) {
+            if (c.wasAdded()) {
+                for (Ennemis a : c.getAddedSubList()
+                ) {
+                    VueEnnemi vueEnm = new VueEnnemi(panneauJeu);
+                    vueEnm.créerSprite(a);
+                }
+            } else if (c.wasRemoved()) {
+                for (Ennemis a : c.getRemoved()
+                ) {
+
+                    jeu.ajoutEnnemisMort(a);
+                    panneauJeu.getChildren().remove((panneauJeu.lookup("#" + a.getId())));
+                }
+            }
+        }
+
+    }
+}
