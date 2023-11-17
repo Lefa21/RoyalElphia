f18c257f4a0eb61fbea44278f2883c0699b9c5e8
alaaeddineb <alaaeddine.b15@gmail.com>
2023-11-17 13:54:42 +0100

    decorateur tour obstacle strategie vague

---
+++ src/main/java/fr/montreuil/iut/RoyalElphia/modele/Obstacle/ObstacleDecorateur.java
@@ -0,0 +1,19 @@
+package fr.montreuil.iut.RoyalElphia.modele.Obstacle;
+
+import fr.montreuil.iut.RoyalElphia.modele.Jeu;
+import fr.montreuil.iut.RoyalElphia.modele.Tour.Tour;
+
+public abstract class ObstacleDecorateur extends Obstacle {
+    protected Obstacle obstacle;
+
+    public ObstacleDecorateur(Obstacle obstacle) {
+        super();
+        this.obstacle = obstacle;
+    }
+
+    public String getChemin() {
+        return null;
+    }
+    public abstract void ameliorationObstacle(Jeu jeu);
+}
+
